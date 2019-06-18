8ed2c63e01c5af0f971b4095fec2ed8e46922504
Ivan Gropetti <ivan.gropetti@gmail.com>
2019-06-18 16:07:58 +0200

    Update script

--- report/customer/invoice/test/ch.banana.uni.invoice.uni10.test.js
+++ report/customer/invoice/test/ch.banana.uni.invoice.uni10.test.js
@@ -16,7 +16,7 @@
 
 // @id = ch.banana.uni.invoice.uni10.test
 // @api = 1.0
-// @pubdate = 2019-05-13
+// @pubdate = 2019-06-18
 // @publisher = Banana.ch SA
 // @description = <TEST ch.banana.uni.invoice.uni10.js>
 // @task = app.command
@@ -29,31 +29,17 @@
 
 
 /*
-
   SUMMARY
   -------
   Javascript test
   1. Open the .ac2 file
   2. Execute the .js script
   3. Save the report
-
-
-
-  virtual void addTestBegin(const QString& key, const QString& comment = QString());
-  virtual void addTestEnd();
-
-  virtual void addSection(const QString& key);
-  virtual void addSubSection(const QString& key);
-  virtual void addSubSubSection(const QString& key);
-
-  virtual void addComment(const QString& comment);
-  virtual void addInfo(const QString& key, const QString& value1, const QString& value2 = QString(), const QString& value3 = QString());
-  virtual void addFatalError(const QString& error);
-  virtual void addKeyValue(const QString& key, const QString& value, const QString& comment = QString());
-  virtual void addReport(const QString& key, QJSValue report, const QString& comment = QString());
-  virtual void addTable(const QString& key, QJSValue table, QStringList colXmlNames = QStringList(), const QString& comment = QString());
-
 **/
+
+
+
+
 
 // Register test case to be executed
 Test.registerTestCase(new ReportInvoiceTemplate10());
@@ -87,98 +73,114 @@ ReportInvoiceTemplate10.prototype.testReport = function() {
    
   Test.logger.addComment("Test ch.banana.uni.invoice.uni10.js");
 
-  var fileAC2 = "file:script/../test/testcases/invoices_universal.ac2";
+  var fileAC2 = "file:script/../test/testcases/invoice_development_file.ac2";
   var banDoc = Banana.application.openDocument(fileAC2);
   
-  //Banana.console.log("bandoc : " + banDoc);
-  if (!banDoc) {return;}
+  if (!banDoc) {
+    return;
+  }
 
   Test.logger.addSection("Invoice tests");
 
   //Invoice 35
   Test.logger.addSubSection("Test Invoice 35");
-  aggiungiReport(banDoc, "35", "Test Invoice 35");
+  addReport(banDoc, "35", "Test Invoice 35");
   
   //Invoice 36
   Test.logger.addSubSection("Test Invoice 36");
-  aggiungiReport(banDoc, "36", "Test Invoice 36");
+  addReport(banDoc, "36", "Test Invoice 36");
 
   //Invoice 37
   Test.logger.addSubSection("Test Invoice 37");
-  aggiungiReport(banDoc, "37", "Test Invoice 37");
+  addReport(banDoc, "37", "Test Invoice 37");
 
   //Invoice 47
   Test.logger.addSubSection("Test Invoice 47");
-  aggiungiReport(banDoc, "47", "Test Invoice 47");
+  addReport(banDoc, "47", "Test Invoice 47");
 }
 
-//Function that creates the report for the test
-function aggiungiReport(banDoc, invoiceNumber, reportName) {
-  
-  var lang = 'en';
-  if (banDoc.locale)
-    lang = banDoc.locale;
-  if (lang.length > 2)
-    lang = lang.substr(0, 2);
-  var texts = setInvoiceTexts(lang);
 
-  //Set params (normally are taken from settings)
-  var param = {};
+//Function used to set all the parameters
+function setParametres(texts) {
+  
+  var userParam = {};
 
   //Include
-  param.print_header = true;
-  param.print_logo = true;
-  param.info_invoice_number = true;
-  param.info_date = true;
-  param.info_customer = true;
-  param.info_due_date = true;
-  param.info_page = true;
-  param.items_description = true;
-  param.items_quantity = true;
-  param.items_unit_price = true;
-  param.items_total = true;
-  
+  userParam.print_header = true;
+  userParam.header_left = false;
+  userParam.header_row_1 = "Banana.ch SA";
+  userParam.header_row_2 = "Via alla Santa 7 - 6962 Viganello";
+  userParam.header_row_3 = "www.banana.ch - info@banana.ch";
+  userParam.header_row_4 = "IVA 123 456 789";
+  userParam.header_row_5 = "";
+  userParam.print_logo = true;
+  userParam.logo_name = 'Logo';
+  userParam.small_address_line = 'Banana.ch SA - Via alla Santa 7 - 6962 Viganello';
+  userParam.address_left = false;
+  userParam.shipping_address = true;
+  userParam.info_invoice_number = true;
+  userParam.info_date = true;
+  userParam.info_customer = true;
+  userParam.info_customer_vat_number = true;
+  userParam.info_customer_fiscal_number = true;
+  userParam.info_due_date = true;
+  userParam.info_page = true;
+  userParam.items_invoice_details = texts.description+";"+texts.quantity+";"+texts.reference_unit+";"+texts.unit_price+";"+texts.amount;
+  userParam.items_invoice_details_dimensions = '50%;10%;10%;15%;15%';
+  userParam.details_gross_amounts = false;
+  userParam.add_footer = true;
+  userParam.footer_left = texts.invoice;
+  userParam.footer_center = '';
+  userParam.footer_right = texts.page+' &['+texts.page+']';
+  userParam.add_qr_code = true;
+  userParam.qr_code_position = 'right';
+  userParam.use_different_address = false;
+  userParam.address_row_1 = '';
+  userParam.address_row_2 = '';
+  userParam.address_row_3 = '';
+
   //Texts
-  param.header_row_1 = "";
-  param.header_row_2 = "";
-  param.header_row_3 = "";
-  param.header_row_4 = "";
-  param.header_row_5 = "";
-  param.info_invoice_number_text = texts.invoice;
-  param.info_date_text = texts.date;
-  param.info_customer_text = texts.customer;
-  param.info_due_date_text = texts.payment_terms_label;
-  param.info_page_text = texts.page;
-  param.items_description_text = texts.description;
-  param.items_quantity_text = texts.qty;
-  param.items_unit_price_text = texts.unit_price;
-  param.items_total_text = texts.total;
+  userParam.add_language = "";
+  userParam.texts_info_invoice_number_text = texts.invoice;
+  userParam.texts_info_date_text = texts.date;
+  userParam.texts_info_customer_text = texts.customer;
+  userParam.texts_info_customer_vat_number = texts.vat_number;
+  userParam.texts_info_customer_fiscal_number = texts.fiscal_number;
+  userParam.texts_info_due_date_text = texts.payment_terms_label;
+  userParam.texts_info_page_text = texts.page;
+  userParam.texts_shipping_address = texts.shipping_address;
+  userParam.title_doctype_10 = texts.invoice + " <DocInvoice>";
+  userParam.title_doctype_12 = texts.credit_note + " <DocInvoice>";
+  userParam.texts_items_details_columns = texts.description+";"+texts.quantity+";"+texts.reference_unit+";"+texts.unit_price+";"+texts.amount;
+  userParam.texts_total = texts.total;
 
   //Styles
-  param.color_1 = '#337ab7';
-  param.color_2 = '#ffffff';
-  param.color_3 = '';
-  param.color_4 = '';
-  param.font_family = 'Helvetica';
-
-  //Invoice elements
-  param.small_address_line = '';
-  param.address_left = false;
-  param.invoice_details_iva_netto = false;
-  param.invoice_details_iva_lordo = false;
-  param.invoice_details_senza_iva = false;
-  param_footer = '';
+  userParam.background_color_1 = '#337ab7';
+  userParam.color = '#ffffff';
+  userParam.background_color_2 = '#F0F8FF';
+  userParam.color_4 = '';
+  userParam.font_family = 'Helvetica';
+  userParam.font_size = '10';
+  userParam.address_margin_left = '113';
+  userParam.address_margin_top = '45';
 
   //Embedded JavaScript file
-  param.custom_javascript_filename = '';
+  userParam.embedded_javascript_filename = '';
+
+  return userParam;
+}
 
 
+//Function that creates the report for the test
+function addReport(banDoc, invoiceNumber, reportName) {
 
-
-  //Banana.console.log(JSON.stringify(jsonInvoice, "", ""));
-
+  var texts = setInvoiceTexts('en');
+  var userParam = setParametres(texts);
+  
   var jsonInvoice = getJsonInvoice(invoiceNumber);
-  var invoiceReport = printInvoice(jsonInvoice, invoiceReport, param);
+  var invoiceObj = JSON.parse(jsonInvoice);
+  
+  var invoiceReport = printInvoice(banDoc, invoiceReport, texts, userParam, "", invoiceObj);
 
   Test.logger.addReport(reportName, invoiceReport);
 }
