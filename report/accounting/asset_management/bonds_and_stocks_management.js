a272f0af1867c89040e13b4fd41d399f96928727
PloszajAaron <aaron.ploszaj@banana.ch>
2021-04-21 16:59:51 +0200

    Update bonds and stocks management extension

--- report/accounting/asset_management/bonds_and_stocks_management.js
+++ report/accounting/asset_management/bonds_and_stocks_management.js
@@ -34,11 +34,14 @@ function addTableBaSTransactions(report) {
     var tableHeader = table_bas_transactions.getHeader();
     var tableRow = tableHeader.addRow();
     tableRow.addCell("Action", "styleTablesHeaderText");
-    tableRow.addCell("Notes", "styleTablesHeaderText");
     tableRow.addCell("Description", "styleTablesHeaderText");
     tableRow.addCell("Quantity", "styleTablesHeaderText");
-    tableRow.addCell("AmountCurrency", "styleTablesHeaderText");
-    tableRow.addCell("Amount", "styleTablesHeaderText");
+    tableRow.addCell("Course ", "styleTablesHeaderText");
+    tableRow.addCell("Amount Currency", "styleTablesHeaderText");
+    tableRow.addCell("Balance Currency", "styleTablesHeaderText");
+    tableRow.addCell("Exchange Rate", "styleTablesHeaderText");
+    tableRow.addCell("Amount CHF", "styleTablesHeaderText");
+    tableRow.addCell("Balance CHF", "styleTablesHeaderText");
     //this.generateHeaderColumns(tableRow);
     return table_bas_transactions;
 }
@@ -51,12 +54,14 @@ function addTableBaSDetails(report) {
     var tableRow = tableHeader.addRow();
     tableRow.addCell("Quantity", "styleTablesHeaderText");
     tableRow.addCell("Purchase Cost", "styleTablesHeaderText");
-    tableRow.addCell("Resale Cost, da generare il numero a dipendenza dei movimenti", "styleTablesHeaderText");
+    tableRow.addCell("Resale Cost", "styleTablesHeaderText");
     //this.generateHeaderColumns(tableRow);
     return table_bas_details;
 }
 
 function printReport() {
+
+    getBuyCoursesAverage();
 
     /**********************************************************
      * create the report and add header and footer
@@ -65,29 +70,44 @@ function printReport() {
     addHeader(report);
     addFooter(report);
 
-    //save the transactions data
-    bas_transactions = loadBasTransactions()
+    bas_data = loadBasData();
 
     /**********************************************************
      * add the Bonds and Stocks transactions table
      **********************************************************/
     var table_bas_transactions = addTableBaSTransactions(report);
-    for (var i = 0; i <= bas_transactions.length - 1; i++) {
-        for (var j = 0; j <= bas_transactions.length - 1; j++) {
-            let action = bas_transactions[i][j].action;
-            let qt = bas_transactions[i][j].qt;
-            let notes = bas_transactions[i][j].notes;
-            let description = bas_transactions[i][j].description;
-            let amount_currency = bas_transactions[i][j].amount_currency;
-            let amount = bas_transactions[i][j].amount;
+
+    for (var key in bas_data) {
+
+        let bas_names = getBasNames();
+        //add the header from the bas names inserted ba the user
+        let tableRow = table_bas_transactions.addRow("styleTablesBasNames");
+        tableRow.addCell(bas_names[key]);
+
+        let rows = bas_data[key];
+        for (var index in rows) {
+            Banana.console.debug(rows[index].qt_cum);
+            let action = rows[index].action;
+            let description = rows[index].description;
+            let qt = rows[index].qt;
+            let course = rows[index].course;
+            let amount_currency = rows[index].amount_currency;
+            let balance_currency = rows[index].balance_currency;
+            let exchange_rate = rows[index].exchange_rate;
+            let amount_chf = rows[index].amount_chf;
+            let balance_resale = rows[index].balance;
             let tableRow = table_bas_transactions.addRow("styleTablRows");
             tableRow.addCell(action);
-            tableRow.addCell(notes);
             tableRow.addCell(description);
-            tableRow.addCell(Banana.SDecimal.round(qt, { 'decimals': 0 }));
+            tableRow.addCell(Banana.SDecimal.round(qt, { 'decimals': 0 }), 'styleNormalAmount');
+            tableRow.addCell(Banana.SDecimal.round(course, { 'decimals': 2 }), 'styleNormalAmount');
             tableRow.addCell(toLocaleAmountFormat(amount_currency), "styleNormalAmount");
-            tableRow.addCell(toLocaleAmountFormat(amount), "styleNormalAmount");
+            tableRow.addCell(toLocaleAmountFormat(balance_currency), "styleNormalAmount");
+            tableRow.addCell(Banana.SDecimal.round(exchange_rate, { 'decimals': 2 }), 'styleNormalAmount');
+            tableRow.addCell(toLocaleAmountFormat(amount_chf), "styleNormalAmount");
+            tableRow.addCell(toLocaleAmountFormat(balance_resale), "styleNormalAmount");
         }
+
     }
 
 
@@ -97,15 +117,25 @@ function printReport() {
      **********************************************************/
     var table_bas_details = addTableBaSDetails(report);
 
-    var tableRow = table_bas_details.addRow("styleTablRows");
-    for (var i = 0; i <= bas_transactions.length - 1; i++) {
-        for (var j = 0; j <= bas_transactions.length - 1; j++) {
-            var tableRow = table_bas_details.addRow();
-            let qt = bas_transactions[i][j].qt;
-            let balance = bas_transactions[i][j].balance // bonds and stocks resale value
-            tableRow.addCell(Banana.SDecimal.round(qt, { 'decimals': 0 }));
-            tableRow.addCell(balance);
-        }
+    for (var i = 0; i < bas_data.length; i++) {
+        let bas_names = getBasNames();
+        let bas_closing_courses = getBasClosingCourses();
+        //add the header from the bas names inserted ba the user
+        let tableRow = table_bas_details.addRow("styleTablesBasNames");
+        tableRow.addCell(bas_names[i]);
+
+        tableRow = table_bas_details.addRow("styleTablesBasResults");
+        tableRow.addCell("Profit(Loss) on sale");
+        tableRow.addCell("Value");
+        tableRow = table_bas_details.addRow("styleTablesBasResults");
+        tableRow.addCell("Profit(Loss) on the course of the day ");
+        tableRow.addCell("Value");
+        tableRow = table_bas_details.addRow("styleTablesBasResults");
+        tableRow.addCell("Income %");
+        tableRow.addCell("Value");
+        tableRow = table_bas_details.addRow("styleTablesBasResults");
+        tableRow.addCell("Average course value");
+        tableRow.addCell(bas_closing_courses[i]);
     }
 
 
@@ -162,10 +192,11 @@ function addFooter(report) {
 }
 
 /**
- * 
+ * Unire i prossimi 3 metodi
  */
-function loadBasTransactions() {
-    var transactions = [];
+
+function loadBasData() {
+    var bas_data = [];
     let userParam = initParam();
     let savedParam = Banana.document.getScriptSettings();
     if (savedParam.length > 0) {
@@ -175,71 +206,114 @@ function loadBasTransactions() {
 
     // for each title account call the method loadCurrentCard
     let value = userParam.bonds_and_stocks_accounts.toString();
-    account_list = value.split(";");
+    let account_list = value.split(";");
     for (var i = 0; i <= account_list.length - 1; i++) {
-        transactions.push(loadCurrentCard(account_list[i]));
+        bas_data.push(loadCurrentCard(account_list[i]));
+        //Banana.console.debug(account_list[i]);
     }
 
 
-    return transactions;
+    return bas_data;
 
+}
+
+function getBasNames(useParam) {
+    let userParam = initParam();
+    let savedParam = Banana.document.getScriptSettings();
+    if (savedParam.length > 0) {
+        userParam = JSON.parse(savedParam);
+        userParam = verifyParam(userParam);
+    }
+    let value = userParam.bonds_and_stocks_names.toString();
+    let name_list = value.split(";");
+
+    return name_list;
+
+}
+
+function getBasClosingCourses() {
+    let userParam = initParam();
+    let savedParam = Banana.document.getScriptSettings();
+    if (savedParam.length > 0) {
+        userParam = JSON.parse(savedParam);
+        userParam = verifyParam(userParam);
+    }
+    let value = userParam.bonds_and_stocks_closing_courses.toString();
+    let closing_course_list = value.split(";");
+
+    return closing_course_list;
 }
 
 /**
  * 
  * @param {*} account the account identifying the asset in the chart of accounts
  * @returns an array of objects, one object for every transaction concerning  'account'
+ * the data is taken from the account card of each 'account' element
  */
 function loadCurrentCard(account) {
+    var account_data = [];
+    var qt_cum = [];
+    //inizializzare con val di default
     if (account) {
-        var account_transactions = [];
         account_journal = Banana.document.currentCard(account, '', '', null);
-        for (var i = 0; i < account_journal.rowCount; i++) {
+        for (var i = 0; i < account_journal.rowCount - 1; i++) {
+            let transaction = {};
             let tRow = account_journal.row(i);
-            let action = "Action";
-            let qt = tRow.value('Quantity');
-            let notes = tRow.value('Notes');
-            let description = tRow.value('Description');
-            let amount_currency = tRow.value('AmountCurrency');
-            let amount = tRow.value('Amount');
-            //balance is the value for the load Bas resale cost
-            let balance = tRow.value('JBalance');
-            let balance_currency = tRow.value("JBalanceAccountCurrency");
+            transaction.action = tRow.value('DocType');
+            transaction.description = tRow.value('Description');
+            transaction.qt = tRow.value('Quantity');
+            transaction.course = tRow.value('UnitPrice');
+            transaction.amount_currency = tRow.value('AmountCurrency');
+            transaction.balance_currency = tRow.value("JBalanceAccountCurrency");
+            transaction.exchange_rate = tRow.value('ExchangeRate');
+            transaction.amount_chf = tRow.value('Amount');
+            transaction.balance = tRow.value('JBalance');
 
-            Banana.console.debug(JSON.stringify(account_transactions));
-
-            account_transactions.push({ action, qt, notes, description, amount_currency, amount, balance, balance_currency });
-
+            account_data.push(transaction);
 
 
         }
     }
-    return account_transactions;
+
+    return account_data;
+}
+
+/**
+ * Calculate the Profit (or loss) on sale for every account
+ */
+function getBuyCoursesAverage(account) {
+    let bas_data = loadBasData();
+    let buy_courses_sum = "";
+    let buy_courses_total = 0;
+    let buy_courses_average = "";
+
+}
+
+
+function getPreviousQtCum(qt_cum, qt) {
+    let previous_qt_cum = qt_cum.slice(-1);
+    previous_qt_cum = Banana.SDecimal.abs(previous_qt_cum);
+    previous_qt_cum = Banana.SDecimal.add(previous_qt_cum, qt);
+
+    return previous_qt_cum;
+
 }
 
 
 /**
- * 
+ * Calculate the Purchasing cost of a stock.
+ * The difference has to be calculated when the stock quantity decreases, so some stocks are selled.
+ * With this difference it's possibile to see if selling the stock generated a profit
  */
-function loadBaSPurchasingCosts() {
+function setBaSPurchasingCosts(balance_previous, cumulated_qt, qt) {
+
+    let balance_purchasing = Banana.SDecimal.divide(balance_previous, cumulated_qt);
+    balance_purchasing = Banana.SDecimal.divide(balance_purchasing, qt);
+
+    return balance_purchasing;
 
 }
 
-/**
- * 
- * @param account the account identifying the asset in the chart of accounts
- * @returns 
- */
-function loadBaSResaleCosts(account) {
-
-    var account_balances
-        //gli importi equivalgono alla colonna Balance
-        //carico i dati per ogni conto titoli che viene inserito
-
-    return account_balances
-
-
-}
 
 function toLocaleAmountFormat(value) {
     if (!value || value.trim().length === 0)
@@ -256,6 +330,9 @@ function initParam() {
 
     userParam.version = "v1.0";
     userParam.bonds_and_stocks_accounts = '1400;1401';
+    userParam.bonds_and_stocks_names = 'Nestle;Intel';
+    //per il profitto(perdita) non realizzato
+    userParam.bonds_and_stocks_closing_courses = '102;55';
 
     return userParam;
 
@@ -269,6 +346,12 @@ function verifyParam(userParam) {
     if (!userParam.bonds_and_stocks_accounts) {
         userParam.bonds_and_stocks_accounts = '1400;1401';
     }
+    if (!userParam.bonds_and_stocks_names) {
+        userParam.bonds_and_stocks_names = 'Title1;Title2';
+    }
+    if (!userParam.bonds_and_stocks_closing_courses) {
+        userParam.bonds_and_stocks_closing_courses = '102;55';
+    }
 
     return userParam;
 
@@ -281,18 +364,44 @@ function verifyParam(userParam) {
 function convertParam(userParam) {
 
     var convertedParam = {};
+    var defaultParam = initParam();
     convertedParam.version = '1.0';
     /* array of script's parameters */
     convertedParam.data = [];
 
     var currentParam = {};
-    currentParam.name = 'bonds_and_stocks';
-    currentParam.title = 'Bonds and Stocks';
+    currentParam.name = 'bonds_and_stocks_accounts';
+    currentParam.title = 'Bonds and Stocks accounts';
     currentParam.type = 'string';
     currentParam.value = userParam.bonds_and_stocks_accounts ? userParam.bonds_and_stocks_accounts : '';
+    currentParam.defaultvalue = defaultParam.bonds_and_stocks_accounts;
     currentParam.editable = true;
     currentParam.readValue = function() {
-        userParam.include = this.value;
+        userParam.bonds_and_stocks_accounts = this.value;
+    }
+    convertedParam.data.push(currentParam);
+
+    var currentParam = {};
+    currentParam.name = 'bonds_and_stocks_names';
+    currentParam.title = 'Bonds and Stocks names';
+    currentParam.type = 'string';
+    currentParam.value = userParam.bonds_and_stocks_names ? userParam.bonds_and_stocks_names : '';
+    currentParam.defaultvalue = defaultParam.bonds_and_stocks_names;
+    currentParam.editable = true;
+    currentParam.readValue = function() {
+        userParam.bonds_and_stocks_names = this.value;
+    }
+    convertedParam.data.push(currentParam);
+
+    var currentParam = {};
+    currentParam.name = 'bonds_and_stocks_closing_courses';
+    currentParam.title = 'Bonds and Stocks closing courses';
+    currentParam.type = 'string';
+    currentParam.value = userParam.bonds_and_stocks_closing_courses ? userParam.bonds_and_stocks_closing_courses : '';
+    currentParam.defaultvalue = defaultParam.bonds_and_stocks_closing_courses;
+    currentParam.editable = true;
+    currentParam.readValue = function() {
+        userParam.bonds_and_stocks_closing_courses = this.value;
     }
     convertedParam.data.push(currentParam);
 
@@ -308,20 +417,22 @@ function settingsDialog() {
     }
     userParam = verifyParam(userParam);
 
-
-    var dialogTitle = 'Settings';
-    var pageAnchor = 'dlgSettings';
-    var convertedParam = convertParam(userParam);
-    if (!Banana.Ui.openPropertyEditor(dialogTitle, convertedParam, pageAnchor))
-        return false;
-    for (var i = 0; i < convertedParam.data.length; i++) {
-        // Read values to dialogparam (through the readValue function)
-        if (typeof(convertedParam.data[i].readValue) == "function")
-            convertedParam.data[i].readValue();
+    if (typeof(Banana.Ui.openPropertyEditor) !== 'undefined') {
+        var dialogTitle = 'Settings';
+        var pageAnchor = 'dlgSettings';
+        var convertedParam = convertParam(userParam);
+        if (!Banana.Ui.openPropertyEditor(dialogTitle, convertedParam, pageAnchor))
+            return;
+        for (var i = 0; i < convertedParam.data.length; i++) {
+            // Read values to dialogparam (through the readValue function)
+            if (typeof(convertedParam.data[i].readValue) == "function")
+                convertedParam.data[i].readValue();
+        }
     }
 
     var paramToString = JSON.stringify(userParam);
     Banana.document.setScriptSettings(paramToString);
+    return true;
 }
 
 /**
@@ -333,11 +444,30 @@ function exec(inData, options) {
 
     //verificare la licenza
 
+    var userParam = {};
+    if (inData.length > 0) {
+        userParam = JSON.parse(inData);
+        userParam = verifyParam(userParam);
+    } else if (options && options.useLastSettings) {
+        var savedParam = Banana.document.getScriptSettings();
+        if (savedParam.length > 0) {
+            userParam = JSON.parse(savedParam);
+            userParam = verifyParam(userParam);;
+        }
+    } else {
+        if (!settingsDialog())
+            return "@Cancel";
+        var savedParam = Banana.document.getScriptSettings();
+        if (savedParam.length > 0) {
+            userParam = JSON.parse(savedParam);
+            userParam = verifyParam(userParam);
+        }
+    }
+
     if (!Banana.document)
         return "@Cancel";
     var report = printReport();
     var stylesheet = getReportStyle();
-    Banana.Report.preview(report, stylesheet);
     Banana.Report.preview(report, stylesheet);
 
 }
