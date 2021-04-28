250eed9742ab9fc07f3e881b846a8f4892ce9208
PloszajAaron <aaron.ploszaj@banana.ch>
2021-04-28 16:38:23 +0200

    Update work on home construction model extensions
    -home_construction_fund_management_report
    -crea_movimenti_delibere (document change)

---
+++ report/accounting/home_construction_fund_management_report/crea_movimenti_delibere.js
@@ -0,0 +1,132 @@
+// Copyright [2020] [Banana.ch SA - Lugano Switzerland]
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// @api = 1.1
+// @id = crea_movimenti_delibere.js
+// @description = Crea i movimenti delle delibere
+// @task = app.command
+// @doctype = 110.*
+// @publisher = Banana.ch SA
+// @pubdate = 2021-04-28
+// @inputdatasource = none
+// @timeout = -1
+
+/**
+ * initialises the document change structure
+ */
+function initDocument() {
+    var jsonDoc = {};
+    jsonDoc.document = {};
+    jsonDoc.document.dataUnits = [];
+
+    jsonDoc.creator = {};
+    jsonDoc.creator.executionDate = getCurrentDate();
+    jsonDoc.creator.name = Banana.script.getParamValue('id');
+    jsonDoc.creator.version = "1.0";
+
+
+    return jsonDoc;
+
+}
+
+/**
+ * set the row operation, the row fields and the table
+ * @returns 
+ */
+function deliberationRowAppend() {
+
+    //rows
+    let rows = [];
+    let categories_list = loadBudgetColumnValues();
+    let sequence = 1.0;
+    let sequence_incr = 0.1;
+
+    for (var key in categories_list) {
+
+        //row operation
+        let row = {};
+        row.operation = {};
+        row.operation.name = "add";
+        // row.operation.sequence = sequence + sequence_incr;
+
+        row.fields = {};
+        row.fields["Date"] = getCurrentDate();
+        row.fields["Description"] = categories_list[key].description;
+        row.fields["Expenses"] = categories_list[key].budget;
+        row.fields["Category"] = categories_list[key].category;
+        row.fields["Cc1"] = "Lavori non ancora Assegnati";
+
+        rows.push(row);
+    }
+
+    //table
+    var dataUnitTransactions = {};
+    dataUnitTransactions.nameXml = "Budget";
+    dataUnitTransactions.data = {};
+    dataUnitTransactions.data.rowLists = [];
+    dataUnitTransactions.data.rowLists.push({ "rows": rows });
+
+    //document
+    var jsonDoc = initDocument();
+    jsonDoc.document.dataUnits.push(dataUnitTransactions);
+
+
+    return jsonDoc;
+
+}
+
+function getCurrentDate() {
+    var d = new Date();
+    var datestring = d.getFullYear() + ("0" + (d.getMonth() + 1)).slice(-2) + ("0" + d.getDate()).slice(-2);
+    return Banana.Converter.toInternalDateFormat(datestring, "yyyymmdd");
+}
+
+function loadBudgetColumnValues() {
+    let categories_list = [];
+
+    if (!Banana.document) {
+        return categories_list;
+    }
+    var table = Banana.document.table("Categories");
+    if (!table) {
+        return categories_list;
+    }
+    for (var i = 2; i < table.rowCount; i++) {
+        let categories = {};
+        var tRow = table.row(i);
+
+        categories.budget = tRow.value("PreventivoGenerale");
+        categories.description = tRow.value("Description");
+        categories.category = tRow.value("Category");
+
+        categories_list.push(categories);
+
+    }
+    return categories_list;
+}
+
+function exec(inData) {
+
+    if (!Banana.document)
+        return "@Cancel"
+
+    var documentChange = { "format": "documentChange", "error": "", "data": [] };
+
+    //1. Appends a row to the transaction table
+    var jsonDoc = deliberationRowAppend();
+    documentChange["data"].push(jsonDoc);
+    // Banana.console.debug(JSON.stringify(documentChange));
+
+    return documentChange;
+}
--- report/accounting/home_construction_fund_management_report/home_construction_fund_management_report.js
+++ report/accounting/home_construction_fund_management_report/home_construction_fund_management_report.js
@@ -30,34 +30,25 @@
 
 function addTableFundManagement(report) {
 
-    //altre colonne verranno aggiunte con il document Change
-    let new_budget_columns = addBudgetColumn_columns();
-
 
     var tableFundManagement = report.addTable('myTableFundManagement');
     tableFundManagement.setStyleAttributes("width:100%;");
     tableFundManagement.getCaption().addText(qsTr("GESTIONE FONDI COSTRUZIONE"), "styleTitles");
     tableFundManagement.addColumn("Categoria").setStyleAttributes("width:20%");
-    tableFundManagement.addColumn("Uscite").setStyleAttributes("width:10%");
-    tableFundManagement.addColumn("Delibere").setStyleAttributes("width:10%");
-    tableFundManagement.addColumn("Delibere-Uscite").setStyleAttributes("width:15%");
     tableFundManagement.addColumn("Preventivo di Massima").setStyleAttributes("width:15%");
+    tableFundManagement.addColumn("Delibere").setStyleAttributes("width:10%");
+    tableFundManagement.addColumn("Uscite Effettive").setStyleAttributes("width:10%");
+    tableFundManagement.addColumn("Delibere-Uscite").setStyleAttributes("width:15%");
     tableFundManagement.addColumn("Delibere-Preventivo di massima").setStyleAttributes("width:15%");
-    for (var key in new_budget_columns) {
-        tableFundManagement.addColumn("New Budget Columns").setStyleAttributes("width:15%");
-    }
 
     var tableHeader = tableFundManagement.getHeader();
     var tableRow = tableHeader.addRow();
     tableRow.addCell("Categoria", "styleTablesHeaderText");
-    tableRow.addCell("Uscite", "styleTablesHeaderText");
-    tableRow.addCell("Delibere", "styleTablesHeaderText");
-    tableRow.addCell("Delibere-Uscite", "styleTablesHeaderText");
     tableRow.addCell("Preventivo di Massima", "styleTablesHeaderText");
+    tableRow.addCell("Delibere", "styleTablesHeaderText");
+    tableRow.addCell("Uscite Effettive", "styleTablesHeaderText");
+    tableRow.addCell("Delibere-Uscite", "styleTablesHeaderText");
     tableRow.addCell("Delibere-Preventivo di massima", "styleTablesHeaderText");
-    for (var key in new_budget_columns) {
-        tableRow.addCell(new_budget_columns[key].nameXml, "styleTablesHeaderText");
-    }
 
     return tableFundManagement;
 }
@@ -72,60 +63,35 @@ function printReport() {
 
     let group_list = loadGroups();
     for (var i = 0; i < group_list.length - 1; i++) {
-        let rows = loadCategoryTableRows(group_list[i]);
+        //inserisco i valori normali
+        let rows = loadCategoriesTableRows(group_list[i]);
         for (var key in rows) {
             let tableRow = tableFundManagement.addRow("styleTablRows");
             tableRow.addCell(rows[key].category, 'styleTablRows');
-            tableRow.addCell(toLocaleAmountFormat(rows[key].expenses), 'styleNormalAmount');
-            tableRow.addCell(toLocaleAmountFormat(rows[key].deliberations), 'styleNormalAmount');
-            cell = tableRow.addCell(toLocaleAmountFormat(rows[key].deliberations_expenses) + ' ', 'styleNormalAmount');
-            addSymbol(rows[key].deliberations_expenses, cell);
             tableRow.addCell(toLocaleAmountFormat(rows[key].budget), 'styleNormalAmount');
+            tableRow.addCell(toLocaleAmountFormat(rows[key].deliberations), 'styleNormalAmount');
+            tableRow.addCell(toLocaleAmountFormat(rows[key].expenses), 'styleNormalAmount');
+            cell = tableRow.addCell(toLocaleAmountFormat(rows[key].deliberations_expenses) + ' ', 'styleNormalAmount');
+            addSymbol(rows[key].deliberations_expenses, cell, false);
             cell = tableRow.addCell(toLocaleAmountFormat(rows[key].deliberations_budget) + ' ', 'styleNormalAmount');
-            addSymbol(rows[key].deliberations_budget, cell);
+            addSymbol(rows[key].deliberations_budget, cell, false);
         }
-        let tableRow = tableFundManagement.addRow("styleTablRows");
-        tableRow.addCell("TOTALE", "styleTablRows");
-        tableRow.addCell("totale uscite", "styleTablRows");
-        tableRow.addCell("totale delibere", "styleTablRows");
-        tableRow.addCell("totale differenza", "styleTablRows");
-        tableRow.addCell("totale preventivo di massima", "styleTablRows");
-        tableRow.addCell("totale differenza", "styleTablRows");
+        //inserisco i totali alla fine di ogni gruppo
+        let totals = loadCategoriesTableTotals(group_list[i]);
+        for (var key in totals) {
+            let tableRow = tableFundManagement.addRow("styleTablRows");
+            tableRow.addCell(totals[key].description, 'styleTablesTotalsDescriptions');
+            tableRow.addCell(toLocaleAmountFormat(totals[key].total_budget), 'styleTotalAmount');
+            tableRow.addCell(toLocaleAmountFormat(totals[key].total_deliberations), 'styleTotalAmount');
+            tableRow.addCell(toLocaleAmountFormat(totals[key].total_expenses), 'styleTotalAmount');
+            cell = tableRow.addCell(toLocaleAmountFormat(totals[key].total_deliberations_expenses) + ' ', 'styleTotalAmount');
+            addSymbol(totals[key].total_deliberations_expenses, cell, true);
+            cell = tableRow.addCell(toLocaleAmountFormat(totals[key].total_deliberations_budget) + ' ', 'styleTotalAmount');
+            addSymbol(totals[key].total_deliberations_budget, cell, true);
+        }
 
 
     }
-
-
-    /*  let categories = loadCategoryTableData("Category", false);
-    let expenses = loadCategoryTableData("Expenses", true);
-    //con il nuovo formato questa colonna contiene i valori delle delibere
-    let deliberations = loadCategoryTableData("Budget", true);
-    let exp_delib_difference = getExpAndDelibDifference(expenses, deliberations);
-    let budget = loadCategoryTableData("PreventivoGenerale", true);
-    let delib_budg_difference = getExpAndBudgDifference(deliberations, budget);
-    //altre colonne verranno aggiunte con il document Change
-    let new_budget_columns = addBudgetColumn_columns();
-
-    var report = Banana.Report.newReport("Gestione delle");
-    addHeader(report);
-    addFooter(report);
-
-    //add the table
-    var tableFundManagement = addTableFundManagement(report);
-    for (var i = 0; i < categories.length - 1; i++) {
-        let tableRow = tableFundManagement.addRow("styleTablRows");
-        tableRow.addCell(categories[i]);
-        tableRow.addCell(toLocaleAmountFormat(expenses[i]), 'styleNormalAmount');
-        tableRow.addCell(toLocaleAmountFormat(deliberations[i]), 'styleNormalAmount');
-        cell = tableRow.addCell(toLocaleAmountFormat(exp_delib_difference[i]) + ' ', 'styleNormalAmount');
-        addSymbol(exp_delib_difference[i], cell);
-        tableRow.addCell(toLocaleAmountFormat(budget[i]), 'styleNormalAmount');
-        cell = tableRow.addCell(toLocaleAmountFormat(delib_budg_difference[i]) + ' ', 'styleNormalAmount');
-        addSymbol(delib_budg_difference[i], cell);
-    }
-
-
-*/
 
     return report;
 
@@ -215,7 +181,7 @@ function loadGroups() {
  * @param {*} importo 
  * @returns 
  */
-function loadCategoryTableRows(group) {
+function loadCategoriesTable() {
 
     var element_list = [];
     if (!Banana.document) {
@@ -226,6 +192,15 @@ function loadCategoryTableRows(group) {
     if (!table) {
         return element_list;
     }
+
+    return table;
+}
+
+
+
+function loadCategoriesTableRows(group) {
+    let table = loadCategoriesTable();
+    let categories_table_rows = [];
 
     //mettere nella doc che le prime due rige vanno lasciate libere come da modello
     for (var i = 2; i < table.rowCount; i++) {
@@ -238,13 +213,37 @@ function loadCategoryTableRows(group) {
             categories.deliberations = tRow.value("Budget");
             categories.deliberations_expenses = getExpAndDelibDifference(categories.expenses, categories.deliberations);
             categories.budget = tRow.value("PreventivoGenerale");
-            categories.deliberations_budget = getExpAndBudgDifference(categories.expenses, categories.budget);
+            categories.deliberations_budget = getDelibAndBudgDifference(categories.deliberations, categories.budget);
             categories.gr = group;
-            element_list.push(categories);
+            categories_table_rows.push(categories);
         }
     }
 
-    return element_list;
+    return categories_table_rows;
+
+}
+
+function loadCategoriesTableTotals(group) {
+    let table = loadCategoriesTable();
+    let categories_table_totals = [];
+
+    for (var i = 0; i < table.rowCount; i++) {
+        let tRow = table.row(i);
+        let group_column = tRow.value("Group");
+        if (group_column === group) {
+            let categories_group_total = {};
+            categories_group_total.description = tRow.value("Description");
+            categories_group_total.total_expenses = tRow.value("Expenses");
+            categories_group_total.total_deliberations = tRow.value("Budget");
+            categories_group_total.total_deliberations_expenses = getExpAndDelibDifference(categories_group_total.total_expenses, categories_group_total.total_deliberations);
+            categories_group_total.total_budget = tRow.value("PreventivoGenerale");
+            categories_group_total.total_deliberations_budget = getDelibAndBudgDifference(categories_group_total.total_deliberations, categories_group_total.total_budget);
+            categories_group_total.group_column = group;
+            categories_table_totals.push(categories_group_total);
+        }
+    }
+
+    return categories_table_totals;
 }
 
 /**
@@ -270,7 +269,7 @@ function getExpAndDelibDifference(expense, deliberation) {
  * 
  * *(i)For ever Budget columns
  */
-function getExpAndBudgDifference(deliberation, budget) {
+function getDelibAndBudgDifference(deliberation, budget) {
     let result = "";
     //for calculation I remove the sign from the deliberations, if it is present
     if (deliberation.indexOf("-" >= 0)) {
@@ -289,84 +288,22 @@ function getExpAndBudgDifference(deliberation, budget) {
  * @param {*} cell the cell with the result
  * @returns 
  */
-function addSymbol(value, cell) {
+function addSymbol(value, cell, total) {
     //var rateOfGrowth = this.setRateOfGrowth(indexT1, indexT2);
     var symbol = '●';
+    let symbolGreen = "symbolGreen";
+    let symbolRed = "symbolRed";
+    let symbolOrange = "symbolOrange";
+
 
     if (value > 0) {
-        cell.addText(symbol, "symbolGreen");
+        cell.addText(symbol, symbolGreen);
     } else if (value < 0) {
-        cell.addText(symbol, "symbolRed");
+        cell.addText(symbol, symbolRed);
     } else {
-        cell.addText(symbol, "symbolOrange");
+        cell.addText(symbol, symbolOrange);
     }
     return;
-}
-
-/**
- * initialises the document change structure
- */
-function initDocument() {
-    var jsonDoc = {};
-    jsonDoc.document = {};
-    jsonDoc.document.dataUnits = [];
-
-    jsonDoc.creator = {};
-    jsonDoc.creator.name = Banana.script.getParamValue('id');
-    jsonDoc.creator.version = "1.0";
-
-    return jsonDoc;
-}
-
-/**
- * Se l'utente lo ha scelto, aggiunge una nuova colonna per il preventivo al file
- */
-function addBudgetColumn_columns() {
-
-    let userParam = initParam();
-    let savedParam = Banana.document.getScriptSettings();
-    if (savedParam.length > 0) {
-        userParam = JSON.parse(savedParam);
-        userParam = verifyParam(userParam);
-    }
-    var columns = [];
-    for (var i = 0; i <= userParam.new_budget_columns - 1; i++) {
-        //column operation
-        let column = {};
-        column.operation = {};
-        column.operation.name = "add";
-
-        //column name Xml
-        column.nameXml = "Preventivo" + i;
-
-        //column Header
-        column.header1 = "Preventivo" + i;
-
-        columns.push(column);
-    }
-
-    return columns
-}
-
-function addBudgetColumn_dataUnits(columns) {
-
-    //data Units
-    let dataUnitsCategories = {};
-    dataUnitsCategories.id = "Categories";
-    dataUnitsCategories.nameXml = "Categories";
-    dataUnitsCategories.nid = "100";
-
-    dataUnitsCategories.data = {};
-    dataUnitsCategories.data.viewList = {};
-    dataUnitsCategories.data.viewList.views = [];
-    dataUnitsCategories.data.viewList.views.push({ "columns": columns, "id": "Base", "nameXml": "Base", "nid": "1" });
-
-    //document
-    let jsonDoc = initDocument();
-    jsonDoc.document.dataUnits.push(dataUnitsCategories);
-
-    return jsonDoc;
-
 }
 
 function initParam() {
@@ -466,25 +403,5 @@ function exec(inData, options) {
     var report = printReport();
     var stylesheet = getReportStyle();
     Banana.Report.preview(report, stylesheet);
-
-    let userParam = initParam();
-    let savedParam = Banana.document.getScriptSettings();
-    if (savedParam.length > 0) {
-        userParam = JSON.parse(savedParam);
-        userParam = verifyParam(userParam);
-    }
-    if (userParam.new_budget_columns > 0) {
-
-        var documentChange = { "format": "documentChange", "error": "", "data": [] };
-
-        //Appends a budget column to the categories table
-        let jsonDoc_columns = addBudgetColumn_columns();
-        let jsonDoc = addBudgetColumn_dataUnits(jsonDoc_columns);
-        documentChange["data"].push(jsonDoc);
-
-        // Banana.console.debug(JSON.stringify(documentChange));
-
-        return documentChange;
-    }
 
 }
