6d6895495f59de7e7d1bb5db5f59f8f4dbd79690
PloszajAaron <aaron.ploszaj@banana.ch>
2021-06-04 11:27:42 +0200

    adds extensions for the constructions model

---
+++ constructions/extensions/construction_project_statement/translations/translations_it.ts
@@ -0,0 +1,246 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>construction_project_report</name>
+    <message>
+        <location filename="../construction_project_report.js" line="307"/>
+        <source>Category</source>
+        <translation>Categoria</translation>
+    </message>
+    <message>
+        <source>Categoryy</source>
+        <translation type="vanished">xxxx</translation>
+    </message>
+    <message>
+        <location filename="../construction_project_report.js" line="308"/>
+        <source>Categories</source>
+        <translation>Categorie</translation>
+    </message>
+    <message>
+        <location filename="../construction_project_report.js" line="309"/>
+        <source>Company</source>
+        <translation>Impresa</translation>
+    </message>
+    <message>
+        <location filename="../construction_project_report.js" line="310"/>
+        <source>Companies</source>
+        <translation>Imprese</translation>
+    </message>
+    <message>
+        <location filename="../construction_project_report.js" line="311"/>
+        <source>Progress %</source>
+        <translation>Progresso %</translation>
+    </message>
+    <message>
+        <location filename="../construction_project_report.js" line="312"/>
+        <source>Outiline Budget</source>
+        <translation>Preventivo di massima</translation>
+    </message>
+    <message>
+        <location filename="../construction_project_report.js" line="313"/>
+        <source>Deliberations</source>
+        <translation>Deliberazioni</translation>
+    </message>
+    <message>
+        <source>Deliberations-Outiline Budget</source>
+        <translation type="vanished">Deliberazioni-Preventivo di massima</translation>
+    </message>
+    <message>
+        <source>Actual Expenses</source>
+        <translation type="vanished">Uscite effettive</translation>
+    </message>
+    <message>
+        <source>Expenses-Outline Budget</source>
+        <translation type="vanished">Uscite-Preventivo di massima</translation>
+    </message>
+    <message>
+        <source>Expenses-Deliberations</source>
+        <translation type="vanished">Uscite-Deliberazioni</translation>
+    </message>
+    <message>
+        <location filename="../construction_project_report.js" line="318"/>
+        <source>Statement by Category</source>
+        <translation>Rapporto per Categoria</translation>
+    </message>
+    <message>
+        <location filename="../construction_project_report.js" line="319"/>
+        <source>Statement by Company</source>
+        <translation>Rapporto per Impresa</translation>
+    </message>
+    <message>
+        <source>Progressssss column</source>
+        <translation type="vanished">Colonna Progresso</translation>
+    </message>
+    <message>
+        <source>Outiline Budget (A)</source>
+        <translation type="vanished">Preventivo di massima (A)</translation>
+    </message>
+    <message>
+        <source>Deliberations (B)</source>
+        <translation type="vanished">Deliberazioni (B)</translation>
+    </message>
+    <message>
+        <source>Actual Expenses (C)</source>
+        <translation type="vanished">Uscite effettive (C)</translation>
+    </message>
+    <message>
+        <location filename="../construction_project_report.js" line="314"/>
+        <source>Expenses</source>
+        <translation>Uscite effettive</translation>
+    </message>
+    <message>
+        <location filename="../construction_project_report.js" line="315"/>
+        <source>Diff.Del./Budg.</source>
+        <translation>Diff. Del./Prev.</translation>
+    </message>
+    <message>
+        <location filename="../construction_project_report.js" line="316"/>
+        <source>Diff.Exp./Budg.</source>
+        <translation>Diff. Eff./Prev.</translation>
+    </message>
+    <message>
+        <location filename="../construction_project_report.js" line="317"/>
+        <source>Diff.Exp./Del.</source>
+        <translation>Diff. Eff./Del.</translation>
+    </message>
+    <message>
+        <location filename="../construction_project_report.js" line="322"/>
+        <source>Progress column</source>
+        <translation>Colonna Progresso</translation>
+    </message>
+    <message>
+        <location filename="../construction_project_report.js" line="323"/>
+        <source>Outline Budget column</source>
+        <translation>Colonna Preventivo di massima</translation>
+    </message>
+    <message>
+        <location filename="../construction_project_report.js" line="324"/>
+        <source>Deliberations column</source>
+        <translation>Colonna Deliberazioni</translation>
+    </message>
+    <message>
+        <location filename="../construction_project_report.js" line="325"/>
+        <source>Deliberations and Outline Budget difference column</source>
+        <translation>Colonna differenza Deliberazioni e Preventivo di massima</translation>
+    </message>
+    <message>
+        <location filename="../construction_project_report.js" line="326"/>
+        <source>Expenses column</source>
+        <translation>Colonna Spese effetive</translation>
+    </message>
+    <message>
+        <location filename="../construction_project_report.js" line="327"/>
+        <source>Expenses and Outline Budget difference column</source>
+        <translation>Colonna differenza Spese e Preventivo di massima</translation>
+    </message>
+    <message>
+        <location filename="../construction_project_report.js" line="328"/>
+        <source>Expenses and Deliberations difference column</source>
+        <translation>Colonna differenza Spese e Deliberazioni</translation>
+    </message>
+</context>
+<context>
+    <name>construction_project_statement_report</name>
+    <message>
+        <source>Category</source>
+        <translation type="vanished">Categoria</translation>
+    </message>
+    <message>
+        <source>Categories</source>
+        <translation type="vanished">Categorie</translation>
+    </message>
+    <message>
+        <source>Company</source>
+        <translation type="vanished">Impresa</translation>
+    </message>
+    <message>
+        <source>Companies</source>
+        <translation type="vanished">Imprese</translation>
+    </message>
+    <message>
+        <source>Progress %</source>
+        <translation type="vanished">Progresso %</translation>
+    </message>
+    <message>
+        <source>Outiline Budget</source>
+        <translation type="vanished">Preventivo di massima</translation>
+    </message>
+    <message>
+        <source>Deliberations</source>
+        <translation type="vanished">Deliberazioni</translation>
+    </message>
+    <message>
+        <source>Deliberations-Outiline Budget</source>
+        <translation type="vanished">Deliberazioni-Preventivo di massima</translation>
+    </message>
+    <message>
+        <source>Actual Expenses</source>
+        <translation type="vanished">Uscite effettive</translation>
+    </message>
+    <message>
+        <source>Expenses-Outline Budget</source>
+        <translation type="vanished">Uscite-Preventivo di massima</translation>
+    </message>
+    <message>
+        <source>Expenses-Deliberations</source>
+        <translation type="vanished">Uscite-Deliberazioni</translation>
+    </message>
+    <message>
+        <source>Statement by Category</source>
+        <translation type="vanished">Rapporto per Categoria</translation>
+    </message>
+    <message>
+        <source>Statement by Company</source>
+        <translation type="vanished">Rapporto per Impresa</translation>
+    </message>
+    <message>
+        <source>Progress column</source>
+        <translation type="vanished">Colonna Progresso</translation>
+    </message>
+    <message>
+        <source>Completion column</source>
+        <translation type="vanished">Colonna Progresso</translation>
+    </message>
+    <message>
+        <source>Outline Budget column</source>
+        <translation type="vanished">Colonna Preventivo di massima</translation>
+    </message>
+    <message>
+        <source>Deliberations column</source>
+        <translation type="vanished">Colonna Deliberazioni</translation>
+    </message>
+    <message>
+        <source>Deliberations and Outline Budget difference column</source>
+        <translation type="vanished">Colonna differenza Deliberazioni e Preventivo di massima</translation>
+    </message>
+    <message>
+        <source>Expenses column</source>
+        <translation type="vanished">Colonna Spese effetive</translation>
+    </message>
+    <message>
+        <source>Expenses and Outline Budget difference column</source>
+        <translation type="vanished">Colonna differenza Spese e Preventivo di massima</translation>
+    </message>
+    <message>
+        <source>Expenses and Deliberations difference column</source>
+        <translation type="vanished">Colonna differenza Spese e Deliberazioni</translation>
+    </message>
+</context>
+<context>
+    <name>create_transactions_deliberations</name>
+    <message>
+        <source>not allocated</source>
+        <translation type="vanished">Non assegnati</translation>
+    </message>
+    <message>
+        <source>Not all</source>
+        <translation type="vanished">Non assengati</translation>
+    </message>
+    <message>
+        <location filename="../create_transactions_deliberations.js" line="47"/>
+        <source>Not allocated</source>
+        <translation>Non assegnati</translation>
+    </message>
+</context>
+</TS>
