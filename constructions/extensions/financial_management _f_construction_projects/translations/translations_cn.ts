c223fcb4e63335edf0f62608eaeaf0c3c9c4f5ec
PloszajAaron <aaron.ploszaj@banana.ch>
2021-08-06 12:39:23 +0200

    Update constructions project
    -rename the "construction project statement" using "financial management of construction projects"
    -rename deliberations with assignments
    -update translations,models,reports and repositories with the new terms

---
+++ constructions/extensions/financial_management _f_construction_projects/translations/translations_cn.ts
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh" sourcelanguage="en">
+<context>
+    <name>create_transactions_for_assignments</name>
+    <message>
+        <location filename="../create_transactions_for_assignments.js" line="47"/>
+        <source>Not assigned</source>
+        <translation type="unfinished">&quot;prova&quot;</translation>
+    </message>
+</context>
+<context>
+    <name>financial_management_of_construction_projects</name>
+    <message>
+        <location filename="../financial_management_of_construction_projects.js" line="307"/>
+        <source>Category</source>
+        <translation>&quot;prova&quot;</translation>
+    </message>
+    <message>
+        <location filename="../financial_management_of_construction_projects.js" line="308"/>
+        <source>Categories</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../financial_management_of_construction_projects.js" line="309"/>
+        <source>Company</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../financial_management_of_construction_projects.js" line="310"/>
+        <source>Companies</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../financial_management_of_construction_projects.js" line="311"/>
+        <source>Progress %</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../financial_management_of_construction_projects.js" line="312"/>
+        <source>Outiline Budget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../financial_management_of_construction_projects.js" line="313"/>
+        <source>Assignments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../financial_management_of_construction_projects.js" line="314"/>
+        <source>Expenses</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../financial_management_of_construction_projects.js" line="315"/>
+        <source>Diff.Assing./Budg.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../financial_management_of_construction_projects.js" line="316"/>
+        <source>Diff.Exp./Budg.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../financial_management_of_construction_projects.js" line="317"/>
+        <source>Diff.Exp./Assing.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../financial_management_of_construction_projects.js" line="318"/>
+        <source>Statement by Category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../financial_management_of_construction_projects.js" line="319"/>
+        <source>Statement by Company</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../financial_management_of_construction_projects.js" line="322"/>
+        <source>Progress column</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../financial_management_of_construction_projects.js" line="323"/>
+        <source>Outline Budget column</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../financial_management_of_construction_projects.js" line="324"/>
+        <source>Assignments column</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../financial_management_of_construction_projects.js" line="325"/>
+        <source>Assignments and Outline Budget difference column</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../financial_management_of_construction_projects.js" line="326"/>
+        <source>Expenses column</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../financial_management_of_construction_projects.js" line="327"/>
+        <source>Expenses and Outline Budget difference column</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../financial_management_of_construction_projects.js" line="328"/>
+        <source>Expenses and Assignments difference column</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
