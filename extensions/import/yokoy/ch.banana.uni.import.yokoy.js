11336b65a1b7e6ddf697287dd4b319dd581bb3f9
PloszajAaron <aaron.ploszaj@banana.ch>
2022-03-07 16:51:07 +0100

    crea progetto estensione yokoy

---
+++ extensions/import/yokoy/ch.banana.uni.import.yokoy.js
@@ -0,0 +1,31 @@
+// Copyright [2022] [Banana.ch SA - Lugano Switzerland]
+//
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//
+// @id = ch.banana.uni.import.yokoy
+// @api = 1.0
+// @pubdate = 2022-03-07
+// @publisher = Banana.ch SA
+// @description = PayPal Import (*.csv)
+// @doctype = *
+// @docproperties =
+// @task = import.transactions
+// @outputformat = tablewithheaders
+// @inputdatasource = openfiledialog
+// @inputfilefilter = Text files (*.txt *.csv);;All files (*.*)
+// @timeout = -1
+
+/**
+ * Parse the yokoy file and and return a.
+ */
