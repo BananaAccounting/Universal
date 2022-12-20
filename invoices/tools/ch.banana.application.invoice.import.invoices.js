467de8e98734d28a2ee2c553999f39040ae25f8d
Joel Mbiye <joembiye@gmail.com>
2022-12-20 11:52:41 +0100

    updated import invoice extension

--- invoices/tools/ch.banana.application.invoice.import.invoices.js
+++ invoices/tools/ch.banana.application.invoice.import.invoices.js
@@ -21,6 +21,106 @@
  * Parse the file and create a document change document to import the imvoices.
  */
 function exec(string) {
+    
+    let banDoc = Banana.document;
+
+    if (!banDoc || string.length <= 0)
+        return "@Cancel";
+
+    if (!verifyBananaVersion())
+        return "@Cancel";
+
+    let jsonDocArray = {};
+    let initJsonDoc = initDocument();
+    let fieldSeparator = findSeparator(string);
+    let transactions = Banana.Converter.csvToArray(string, fieldSeparator, '"');
+    let transactionsHeader = transactions[0];
+    transactions.splice(0, 1);
+    let transactionsObjs = Banana.Converter.arrayToObject(transactionsHeader, transactions, true);
+    
     var documentChange = { "format": "documentChange", "error": "","data":[]};
     return documentChange; 
+}
+
+function initDocument() {
+    let jsonDoc = {};
+    jsonDoc.document = {};
+    jsonDoc.document.dataUnits = [];
+
+    jsonDoc.creator = {};
+    jsonDoc.creator.executionDate = getCurrentDate();
+    jsonDoc.creator.name = Banana.script.getParamValue('id');
+    jsonDoc.creator.version = "1.0";
+
+    return jsonDoc;
+}
+
+function getCurrentDate() {
+    let date = new Date();
+    let dateString = date.getFullYear() + ("0" + (date.getMonth() + 1)).slice(-2) + ("0" + (date.getDate()).slice(-2));
+    return Banana.Converter.toInternalDateFormat(dateString, "yyyymmdd");
+}
+
+let formatInvoices = class formatInvoices {
+
+    constructor(banDocument) {
+        this.placeholder = "";
+        this.invoiceNetTotal = "";
+        this.invoiceNetTotalAfterDisc = "";
+        this.invoiceVatTotal = "";
+        this.NetTotalIsOk = false;
+        this.VatTotalIsOk = false;
+        this.discountTotal = "";
+        this.lang = getLang();
+        this.banDoc = banDocument;
+
+        //error messages
+        this.ID_ERR_AMOUNTS_WITH_DIFFERENCES = "ID_ERR_AMOUNTS_WITH_DIFFERENCES";
+        this.ID_ERR_COSTUMERID_NOT_FOUND = "ID_ERR_COSTUMERID_NOT_FOUND";
+        this.ID_ERR_WRONG_INVOICE_TYPE = "ID_ERR_WRONG_INVOICE_TYPE";
+    }
+
+    /** Return true if the transactions match this format */
+    match(transactions) {
+        if ( transactions.length === 0)
+            return false;
+
+        let formatMatched = false;
+    }
+
+    convertInDocChange(transactionsObjs, initJsonDoc) {
+        let jsonDoc = [];
+        let docInfo = getDocumentInfo();
+        let rows = [];
+        let invoiceObj = {};
+
+        /* Iterate over the rows and create object */
+        for (let trRow in transactionsObjs) {
+            let invoiceTransaction = transactionsObjs[trRow];
+
+            if (this.placeholder !== invoiceTransaction["number"]) {
+                invoiceObj = this.setInvoiceStructure(invoiceTransaction. docInfo);
+                invoiceObj.items = this.setInvoiceStructure_items(transactionsObjs, invoiceTransaction["number"]);
+
+                // Recalculate invoice
+                if(!invoiceObj.billing_info)
+                invoiceObj.billing_info={};
+
+                invoiceObj.billing_info.discount={};
+                if(this.discountTotal=="0"){
+                this.discountTotal=null;
+                }
+                invoiceObj.billing_info.discount.amount_vat_exclusive=this.discountTotal;
+                invoiceObj = JSON.parse(this.banDoc.calculateInvoice(JSON.stringify(invoiceObj)));
+
+                // check that the information in the billing info property coincides with the totals taken from the invoice lines
+                this.checkCalculatedAmounts(invoiceObj);
+
+                let row = {};
+                row.operation = {};
+                row.operation.name = "add";
+                row.fields={};
+            }
+        }
+    }
 }
