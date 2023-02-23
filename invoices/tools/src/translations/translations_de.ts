01b4ed6cc704c17a276492c7f725c252db4e29cd
Lorenzo Sinigaglia <lorenzo@banana.ch>
2023-02-23 15:45:33 +0100

    Update translations

--- invoices/tools/src/translations/translations_de.ts
+++ invoices/tools/src/translations/translations_de.ts
@@ -12,60 +12,60 @@
         <translation type="vanished">ZeilenID ist ein erforderliches Feld</translation>
     </message>
     <message>
-        <location filename="../export_contacts.js" line="71"/>
+        <location filename="../export_contacts.js" line="72"/>
         <source>RowId is a required field</source>
         <translation>RowId ist ein erforderliches Feld</translation>
     </message>
     <message>
-        <location filename="../export_contacts.js" line="75"/>
-        <location filename="../export_contacts.js" line="84"/>
+        <location filename="../export_contacts.js" line="79"/>
+        <location filename="../export_contacts.js" line="97"/>
         <source>Organisation name is a required field</source>
         <translation>OrganisationName ist ein erforderliches Feld</translation>
     </message>
     <message>
-        <location filename="../export_contacts.js" line="77"/>
-        <location filename="../export_contacts.js" line="87"/>
+        <location filename="../export_contacts.js" line="81"/>
+        <location filename="../export_contacts.js" line="105"/>
         <source>FirstName is a required field</source>
         <translation>FirstName ist ein erforderliches Feld</translation>
     </message>
     <message>
-        <location filename="../export_contacts.js" line="79"/>
-        <location filename="../export_contacts.js" line="90"/>
+        <location filename="../export_contacts.js" line="83"/>
+        <location filename="../export_contacts.js" line="113"/>
         <source>LastName is a required field</source>
         <translation>LastName ist ein erforderliches Feld</translation>
     </message>
     <message>
-        <location filename="../export_contacts.js" line="96"/>
+        <location filename="../export_contacts.js" line="124"/>
         <source>Street is a required field</source>
         <translation>Street ist ein erforderliches Feld</translation>
     </message>
     <message>
-        <location filename="../export_contacts.js" line="100"/>
+        <location filename="../export_contacts.js" line="131"/>
         <source>PostalCode is a required field</source>
         <translation>PostalCode ist ein erforderliches Feld</translation>
     </message>
     <message>
-        <location filename="../export_contacts.js" line="104"/>
+        <location filename="../export_contacts.js" line="138"/>
         <source>Locality is a required field</source>
         <translation>Locality ist ein erforderliches Feld</translation>
     </message>
     <message>
-        <location filename="../export_contacts.js" line="108"/>
+        <location filename="../export_contacts.js" line="145"/>
         <source>CountryCode is a required field</source>
         <translation>CountryCode ist ein erforderliches Feld</translation>
     </message>
     <message>
-        <location filename="../export_contacts.js" line="114"/>
+        <location filename="../export_contacts.js" line="153"/>
         <source>Error Description: </source>
         <translation>Fehlerbeschreibung: </translation>
     </message>
     <message>
-        <location filename="../export_contacts.js" line="114"/>
+        <location filename="../export_contacts.js" line="153"/>
         <source>An error occured while exporting the csv contacts! </source>
         <translation>Beim Exportieren der csv-Kontakte ist ein Fehler aufgetreten! </translation>
     </message>
     <message>
-        <location filename="../export_contacts.js" line="122"/>
+        <location filename="../export_contacts.js" line="162"/>
         <source>Complete the missing details first, as listed in the message pane below.</source>
         <translation>Vervollständigen Sie zunächst die fehlenden Angaben, die im nachstehenden Meldungsfenster aufgeführt sind.</translation>
     </message>
@@ -77,44 +77,57 @@
         <translation type="vanished">Rechnungen</translation>
     </message>
     <message>
-        <location filename="../export_invoices.js" line="74"/>
         <source>InvoiceDate is a required field</source>
-        <translation>InvoiceDate ist ein erforderliches Feld</translation>
+        <translation type="vanished">InvoiceDate ist ein erforderliches Feld</translation>
     </message>
     <message>
         <source>Contact Id is a required field</source>
         <translation type="vanished">ContactsId ist ein erforderliches Feld</translation>
     </message>
     <message>
-        <location filename="../export_invoices.js" line="78"/>
         <source>ContactsId is a required field</source>
-        <translation>ContactsId ist ein erforderliches Feld</translation>
+        <translation type="vanished">ContactsId ist ein erforderliches Feld</translation>
     </message>
     <message>
-        <location filename="../export_invoices.js" line="87"/>
         <source>ItemDescription is a required field</source>
-        <translation>ItemDescription, innerhalb der Rechnung, ist ein erforderliches Feld</translation>
+        <translation type="vanished">ItemDescription, innerhalb der Rechnung, ist ein erforderliches Feld</translation>
     </message>
     <message>
-        <location filename="../export_invoices.js" line="93"/>
-        <location filename="../export_invoices.js" line="103"/>
         <source>ItemTotal is a required field</source>
-        <translation>ItemTota, innerhalb der Rechnung, ist ein erforderliches Feld</translation>
+        <translation type="vanished">ItemTota, innerhalb der Rechnung, ist ein erforderliches Feld</translation>
     </message>
     <message>
-        <location filename="../export_invoices.js" line="123"/>
         <source>An error occured while exporting the csv invoice! </source>
-        <translation>Beim Exportieren der csv-Rechnung ist ein Fehler aufgetreten! </translation>
+        <translation type="vanished">Beim Exportieren der csv-Rechnung ist ein Fehler aufgetreten! </translation>
     </message>
     <message>
-        <location filename="../export_invoices.js" line="123"/>
         <source>Error Description: </source>
-        <translation>Fehlerbeschreibung: </translation>
+        <translation type="vanished">Fehlerbeschreibung: </translation>
     </message>
     <message>
-        <location filename="../export_invoices.js" line="130"/>
         <source>Complete the missing details first, as listed in the message pane below.</source>
-        <translation>Vervollständigen Sie zunächst die fehlenden Angaben, die im nachstehenden Meldungsfenster aufgeführt sind.</translation>
+        <translation type="vanished">Vervollständigen Sie zunächst die fehlenden Angaben, die im nachstehenden Meldungsfenster aufgeführt sind.</translation>
+    </message>
+    <message>
+        <location filename="../export_invoices.js" line="33"/>
+        <source>Fix errors first, as listed in the pane Messages.</source>
+        <translation>Beheben Sie zuerst die Fehlern, wie in der Registerkarte Meldungen aufgeführt.</translation>
+    </message>
+    <message>
+        <location filename="../export_invoices.js" line="70"/>
+        <location filename="../export_invoices.js" line="74"/>
+        <location filename="../export_invoices.js" line="83"/>
+        <location filename="../export_invoices.js" line="89"/>
+        <location filename="../export_invoices.js" line="99"/>
+        <source>%1 is a required field</source>
+        <translation>%1 ist ein erforderliches Feld</translation>
+    </message>
+    <message>
+        <location filename="../export_invoices.js" line="121"/>
+        <source>Invoice not valid.
+Error: %1</source>
+        <translation>Rechnung ungultig.
+Fehler: %1</translation>
     </message>
 </context>
 <context>
@@ -128,32 +141,32 @@
         <translation type="vanished">Id ist ein erforderliches Feld</translation>
     </message>
     <message>
-        <location filename="../export_items.js" line="64"/>
+        <location filename="../export_items.js" line="65"/>
         <source>RowId is a required field</source>
         <translation>RowId ist ein erforderliches Feld</translation>
     </message>
     <message>
-        <location filename="../export_items.js" line="67"/>
+        <location filename="../export_items.js" line="71"/>
         <source>Description is a required field</source>
         <translation>Description ist ein erforderliches Feld</translation>
     </message>
     <message>
-        <location filename="../export_items.js" line="70"/>
+        <location filename="../export_items.js" line="77"/>
         <source>UnitPrice is a required field</source>
         <translation>UnitPrice ist ein erforderliches Feld</translation>
     </message>
     <message>
-        <location filename="../export_items.js" line="76"/>
+        <location filename="../export_items.js" line="85"/>
         <source>Error Description: </source>
         <translation>Fehlerbeschreibung: </translation>
     </message>
     <message>
-        <location filename="../export_items.js" line="76"/>
+        <location filename="../export_items.js" line="85"/>
         <source>An error occured while exporting the csv items! </source>
         <translation>Beim Exportieren der csv-Artikel ist ein Fehler aufgetreten! </translation>
     </message>
     <message>
-        <location filename="../export_items.js" line="84"/>
+        <location filename="../export_items.js" line="94"/>
         <source>Complete the missing details first, as listed in the message pane below.</source>
         <translation>Vervollständigen Sie zunächst die fehlenden Angaben, die im nachstehenden Meldungsfenster aufgeführt sind.</translation>
     </message>
@@ -161,44 +174,43 @@
 <context>
     <name>import_invoices</name>
     <message>
-        <location filename="../import_invoices.js" line="208"/>
+        <location filename="../import_invoices.js" line="218"/>
         <source>The calculated amount for invoice %1 is different from the amount in the imported file. Calculated amount %2, amount imported file: %3</source>
-        <translation type="unfinished"></translation>
+        <translation>Der berechnete Betrag für die Rechnung %1 unterscheidet sich von dem Betrag in der importierten Datei. Berechnete Betrag %2, Betrag importierte Datei: %3</translation>
     </message>
     <message>
-        <location filename="../import_invoices.js" line="219"/>
+        <location filename="../import_invoices.js" line="229"/>
         <source>The calculated vat amount for invoice %1 is different from the amount in the imported file. Calculated vat amount %2, amount imported file: %3</source>
-        <translation type="unfinished"></translation>
+        <translation>Der berechnete MwSt-Betrag für die Rechnung %1 unterscheidet sich von dem MwSt-Betrag in der importierten Datei. Berechnete Betrag %2, Betrag importierte Datei: %3</translation>
     </message>
     <message>
-        <location filename="../import_invoices.js" line="231"/>
+        <location filename="../import_invoices.js" line="241"/>
         <source>Invoice</source>
-        <translation type="unfinished"></translation>
+        <translation>Rechnung</translation>
     </message>
     <message>
-        <location filename="../import_invoices.js" line="232"/>
+        <location filename="../import_invoices.js" line="242"/>
         <source>Reference nr: </source>
-        <translation type="unfinished"></translation>
+        <translation>Referenznummer: </translation>
     </message>
     <message>
-        <location filename="../import_invoices.js" line="476"/>
+        <location filename="../import_invoices.js" line="485"/>
+        <source>Contact id &apos;%1&apos; not found in contact table. Did you import the contacts?</source>
+        <translation>Kontakt-ID &apos;%1&apos; in Kontakttabelle nicht gefunden. Haben Sie die Kontakte importiert?</translation>
+    </message>
+    <message>
+        <location filename="../import_invoices.js" line="487"/>
+        <source>The calculated amount is different from the amount in your file, invoice nr: %1</source>
+        <translation>Der berechnete Betrag unterscheidet sich von dem Betrag in Ihrer Datei, Rechnungsnummer: %1</translation>
+    </message>
+    <message>
         <source>Contact id: </source>
-        <translation type="unfinished"></translation>
+        <translation type="vanished">Kontakt-ID: </translation>
     </message>
     <message>
-        <location filename="../import_invoices.js" line="476"/>
-        <source> not found in contact table. Did you import the contacts?</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../import_invoices.js" line="478"/>
-        <source>The calculated amount is different from the amount in your file, invoice nr: </source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../import_invoices.js" line="480"/>
+        <location filename="../import_invoices.js" line="489"/>
         <source>You are trying to import a &apos;Single line&apos; invoice, import a &apos;Detailed&apos; invoice instead.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sie versuchen, eine &quot;einzeilige&quot; Rechnung zu importieren, importieren Sie stattdessen eine &quot;detaillierte&quot; Rechnung.</translation>
     </message>
 </context>
 </TS>
