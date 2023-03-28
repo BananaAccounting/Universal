9dfb51705a613374047595d5b3b309c61737bddb
Joel Mbiye <joembiye@gmail.com>
2023-03-28 09:02:33 +0100

    update export/import invoices

--- invoices/tools/test/import_invoices.test.js
+++ invoices/tools/test/import_invoices.test.js
@@ -1,11 +1,11 @@
-// @id = import_items.test
+// @id = import_invoices.test
 // @api = 1.0
 // @pubdate = 2023-01-20
 // @publisher = Banana.ch SA
-// @description = Import items
-// @description.de = Artikeln importieren
-// @description.fr = Importer articles
-// @description.it = Importa articoli
+// @description = Import invoices
+// @description.de = Rechnungen importieren
+// @description.fr = Importer factures
+// @description.it = Importa fatture
 // @doctype = 400.400
 // @docproperties =
 // @task = import.rows
@@ -19,72 +19,78 @@
 // @includejs = ../src/import_invoices.js
 
 // Register test case to be executed
-Test.registerTestCase(new ImportInvoices());
+Test.registerTestCase(new TestImportInvoices());
 
 // Here we define the class, the name of the class is not important
-function ImportInvoices() {
+function TestImportInvoices() {
 
 }
 
 // This method will be called at the beginning of the test case
-ImportInvoices.prototype.initTestCase = function() {
+TestImportInvoices.prototype.initTestCase = function() {
     this.testLogger = Test.logger;
     this.progressBar = Banana.application.progressBar;
 
-    this.fileAC2 = "file:script/../test/testcases/import_invoices_test.ac2";
-    this.csvItemsFile = "file:script/../test/testcases/items.csv";
+    this.fileAC2 = "file:script/../test/testcases/invoices_testfiles/import_invoices_test.ac2";
+    this.csvInvoicesFile = "file:script/../test/testcases/invoices_testfiles/invoices.csv";
     this.jsonDoc = this.initJson();
 }
 
 // This method will be called at the end of the test case
-ImportInvoices.prototype.cleanupTestCase = function() {
+TestImportInvoices.prototype.cleanupTestCase = function() {
 
 }
 
 // This method will be called before every test method is executed
-ImportInvoices.prototype.init = function() {
+TestImportInvoices.prototype.init = function() {
 
 }
 
 // This method will be called after every test method is executed
-ImportInvoices.prototype.cleanup = function() {
+TestImportInvoices.prototype.cleanup = function() {
 
 }
 
-ImportInvoices.prototype.testImportItems = function() {
+TestImportInvoices.prototype.testImportInvoices = function() {
     this.testLogger.addKeyValue("ImportInvoices", "testReport");
     this.testLogger.addComment("Test Invoices import");
 
     let banDoc = Banana.application.openDocument(this.fileAC2);
-    let file = Banana.IO.getLocalFile(this.csvItemsFile);
+    Test.assert(banDoc, `file not found: "${this.fileAC2}"`);
 
-    if (file) {
-        fileContent = file.read();
-    } else {
-        this.testLogger.addFatalError("File not found: " + fileContent);
+    let file = Banana.IO.getLocalFile(this.csvInvoicesFile);
+    Test.assert(file, `file not found: "${this.csvInvoicesFile}"`);
+
+    fileContent = file.read();
+   
+    banDoc.clearMessages();
+    let jsonDocArray = {};
+    let transactions = Banana.Converter.csvToArray(fileContent, ";", '"');
+    let transactions_header = transactions[0];
+
+    transactions.splice(0, 1);
+    let transactionsObjs = Banana.Converter.arrayToObject(transactions_header, transactions, true);
+    let format_invs = createFormatInvs(banDoc);
+    if (format_invs.match(transactionsObjs)) {
+        let format = format_invs.convertInDocChange(transactionsObjs, this.jsonDoc);
+        jsonDocArray = format;
+    }
+    
+    let documentChange = { "format": "documentChange", "error": "","data":[]};
+    documentChange["data"].push(jsonDocArray);
+
+    let msgs = banDoc.getMessages();
+    for (let i = 0; i < msgs.length; ++i) {
+        let msg = msgs[i];
+        this.testLogger.addKeyValue("ERROR_MSG_ROW_" + msg.rowNr, msg.message);
+        this.testLogger.addKeyValue("ERROR_HELPID_ROW_" + msg.rowNr, msg.helpId);
     }
 
-    if (banDoc) {
-        let jsonDocArray = {};
-        let transactions = Banana.Converter.csvToArray(fileContent, ";", '"');
-        let transactions_header = transactions[0];
-
-        transactions.splice(0, 1);
-        let transactionsObjs = Banana.Converter.arrayToObject(transactions_header, transactions, true);
-        let format_itm = createFormatItm();
-        if (format_itm.match(transactionsObjs)) {
-            let format = format_itm.convertInDocChange(transactionsObjs, this.jsonDoc);
-            jsonDocArray = format;
-        }
-        
-        let documentChange = { "format": "documentChange", "error": "","data":[]};
-        documentChange["data"].push(jsonDocArray);
-
-        return documentChange;
-    }
+    return documentChange;
+    
 }
 
-ImportInvoices.prototype.initJson = function() {
+TestImportInvoices.prototype.initJson = function() {
     let jsonDoc = {};
     jsonDoc.document = {};
     jsonDoc.document.dataUnits = [];
