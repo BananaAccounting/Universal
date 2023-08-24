12941eca8bd4f02f4a1e54d7c725348c7db1ce3c
Patrick Pasquillo <mail@patrickpasquillo.net>
2023-08-24 16:02:12 +0200

    Rents Extension Test Updated

--- realestate/extensions/rentsreport/test/detailedrentsreport.test.js
+++ realestate/extensions/rentsreport/test/detailedrentsreport.test.js
@@ -20,7 +20,14 @@ function TestDetailedReport() {
 
 // This method will be called at the beginning of the test case
 TestDetailedReport.prototype.initTestCase = function () {
+  this.testLogger = Test.logger;
   this.progressBar = Banana.application.progressBar;
+  this.fileAC2Path = [];
+
+  this.fileAC2Path.push("file:script/../test/testcases/AffittiPartitaDoppia.ac2");
+  this.fileAC2Path.push("file:script/../test/testcases/AffittiPartitaDoppiaNOPartitario.ac2");
+  this.fileAC2Path.push("file:script/../test/testcases/AffittiEntrateUscite.ac2");
+  this.fileAC2Path.push("file:script/../test/testcases/AffittiEntrateUsciteNOPartitario.ac2");
 }
 
 // This method will be called at the end of the test case
@@ -35,33 +42,27 @@ TestDetailedReport.prototype.init = function () {
 TestDetailedReport.prototype.cleanup = function () {
 }
 
-TestDetailedReport.prototype.testImport = function () {
-  let fileAC2Path = [];
-
-  fileAC2Path.push("file:script/../test/testcases/AffittiPartitaDoppia.ac2");
-  fileAC2Path.push("file:script/../test/testcases/AffittiPartitaDoppiaNOPartitario.ac2");
-  fileAC2Path.push("file:script/../test/testcases/AffittiEntrateUscite.ac2");
-  fileAC2Path.push("file:script/../test/testcases/AffittiEntrateUsciteNOPartitario.ac2");
-
-
-  let logger = Test.logger;
+TestDetailedReport.prototype.testDetailedReport = function () {
   
-  for (let k = 0; k.lenght; k++) {
+  let parentLogger = this.testLogger;
+  this.progressBar.start(this.fileAC2Path.length);
+  
+  for (let k = 0; k.length; k++) {
+    
+    if (!this.progressBar.step())
+            break;
     let banDoc = Banana.application.openDocument(fileAC2Path[k]);
-
+    this.testLogger = parentLogger.newLogger(Banana.IO.fileCompleteBaseName(fileAC2Path[k]));
+    
     if (banDoc) {
-
       let printreport = new PrintReport(banDoc);
-      var result = printreport.report();
-      logger.addReport("Result detailed report", result);
-      logger.addComment("Test " + (k + 1) + ": " + fileAC2Path[k]);
-      if (!Banana.application.progressBar.step())
-        break;
-
+      let result = printreport.report();
+      let reportName = "FILENAME: " + fileAC2Path[k] + "\n";
+      this.testLogger.addReport(reportName, result, "Test " + (k + 1) + ": " + fileAC2Path[k]);
     } else {
-      logger.addFatalError("No valid file ac2 found in this directory: " + fileAC2Path[k]);
+      this.testLogger.addReport("No valid file ac2 found in this directory: " + fileAC2Path[k]);
     }
   }
 
-  Banana.application.progressBar.finish();
+  this.progressBar.finish();
 }
