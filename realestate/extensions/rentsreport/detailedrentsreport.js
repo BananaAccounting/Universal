bb0acc1f0d8601dd06b3fa4849c516577f47d50d
Patrick Pasquillo <mail@patrickpasquillo.net>
2024-01-31 13:42:17 +0100

    Rents extension: required license fixed

--- realestate/extensions/rentsreport/detailedrentsreport.js
+++ realestate/extensions/rentsreport/detailedrentsreport.js
@@ -21,18 +21,15 @@
 
 //Check if the version of Banana Accounting is compatible
 function verifyBananaAdvancedVersion() {
-  if (!this.banDocument)
-    return false;
+  if (!Banana.document)
+      return false;
 
 
   if (!Banana.application.license || Banana.application.license.licenseType !== "advanced") {
-    var lang = this.getLang();
-    var msg = "This extension requires Banana Accounting+ Advanced";
-    this.banDocument.addMessage(msg, "ID_ERR_LICENSE_NOTVALID");
-    return false;
+      var msg = "This extension requires Banana Accounting+ Advanced";
+      Banana.document.addMessage(msg, "ID_ERR_LICENSE_NOTVALID");
+      return false;
   }
-
-
 
   return true;
 }
