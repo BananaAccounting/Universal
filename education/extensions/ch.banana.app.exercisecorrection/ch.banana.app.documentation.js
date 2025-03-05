ac058ecede085e9a967e8ba12ec50ebdc13a7356
Patrick Pasquillo <mail@patrickpasquillo.net>
2025-03-05 08:28:00 +0100

    Add Extension Update

--- education/extensions/ch.banana.app.exercisecorrection/ch.banana.app.documentation.js
+++ education/extensions/ch.banana.app.exercisecorrection/ch.banana.app.documentation.js
@@ -14,20 +14,6 @@
 // @includejs = ch.banana.app.functions.js
 // @includejs = ch.banana.app.settings.js
 
-//Check if the version of Banana Accounting is compatible
-function verifyBananaAdvancedVersion() {
-    if (!Banana.document)
-        return false;
-
-    if (!Banana.application.license || Banana.application.license.licenseType !== "advanced") {
-        var msg = "This extension requires Banana Accounting+ Advanced";
-        Banana.document.addMessage(msg, "ID_ERR_LICENSE_NOTVALID");
-        return false;
-    }
-
-    return true;
-}
-
 function exec() {
 
     if (!verifyBananaAdvancedVersion()) {
